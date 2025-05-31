4643e8b33f4d58e9054e97ab5d86cd6dfc609c7a
rizwanahmed2002 <irizwanms@gmail.com>
2025-05-31 20:48:40 +0500

    new file:   app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// 1. Ask the user for their age. If the age is 18 or more, show an alert: "You are an
+// adult.", else show: "You are a minor."
+console.log("Hello Dear User!");
+// 2. Ask the user to enter a number. If it's positive, log: "Positive number"; else log:
+// "Not positive".
+
+// 3. Ask the user for a number. If it’s even, alert: "Even number"; else alert: "Odd
+// number".
+
+// 4. Ask for a username. If the username is "admin", display "Welcome, admin!"; else
+// display "Access denied."
+
+// 5. Ask for two numbers. If they are equal, log "Both numbers are the same";
+// otherwise, log "The numbers are different".
+
+// 6. Ask the user to enter two numbers. Check which one is larger and alert: "First is
+// larger" or "Second is larger".
+
+// 7. Ask for a number. If it's greater than 100, alert "Too big!", else alert "Within
+// range".
+
+// 8. Ask for a score. If it’s 50 or higher, log "You passed!", else log "You failed."
+
+// 9. Ask the user to enter a temperature. If it’s over 30, alert "It's hot!"; else alert "It's
+// not hot."
+
+// 10. Ask the user to enter a number. If it's 0, alert "Zero"; if it’s not zero, alert "Not zero".
+
+// 11. Ask the user for their age. If it's 60 or more, log "You are a senior citizen.",
+// else log "You are not a senior."
+
+// 12. Ask the user for their name. If the name is "Alice", alert "Hi, Alice!"; else alert
+// "You're not Alice!"
